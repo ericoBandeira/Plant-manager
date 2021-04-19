21a9ccdd4302d5f74bbfbf7b5951a3f3a7a33915
ericoBandeira <ericobandeira@hotmail.com>
2021-04-19 16:34:55 -0300

    adicionando base do projeto

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+import wateringImg from '../assets/watering.png'
+
+export function Welcome(){
+  return(
+    <View >
+      <Text>
+        Gerencie suas plantas de forma fácil
+      </Text>
+
+      <Image source={wateringImg} />
+
+      <Text>
+
+      </Text>
+    </View>
+  );
+}
+
